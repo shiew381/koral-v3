e5cb98e2f6258476ecbf954a07f237943c71fca8
shiew381 <shiew381@gmail.com>
2023-07-11 15:13:26 -0700

    fix multiple choice grading for many-answers

--- src/utils/gradeMultipleChoice.js
+++ src/utils/gradeMultipleChoice.js
@@ -1,21 +1,41 @@
 export function gradeMultipleChoice(question, response) {
   const pointsPossible = question?.pointsPossible;
 
+  const zeroScore = {
+    answeredCorrectly: false,
+    pointsAwarded: 0,
+  };
+
+  const fullScore = {
+    answeredCorrectly: true,
+    pointsAwarded: pointsPossible,
+  };
+
   const correctChoices = [];
+
   question.answerChoices.forEach((el, ind) => {
     if (el.isCorrect) {
       correctChoices.push(ind);
     }
   });
 
-  const answeredCorrectly = response.every(
-    (el, ind) => el === correctChoices[ind]
-  );
+  if (correctChoices.length !== response.length) {
+    return zeroScore;
+  }
 
-  const pointsAwarded = answeredCorrectly ? pointsPossible : 0;
+  if (correctChoices.length === 1) {
+    const answeredCorrectly = correctChoices[0] === response[0];
 
-  return {
-    answeredCorrectly: answeredCorrectly,
-    pointsAwarded: pointsAwarded,
-  };
+    return answeredCorrectly ? fullScore : zeroScore;
+  }
+
+  if (correctChoices.length > 1) {
+    const answeredCorrectly = response.every(
+      (el, ind) => el === correctChoices[ind]
+    );
+
+    return answeredCorrectly ? fullScore : zeroScore;
+  }
+
+  return;
 }
