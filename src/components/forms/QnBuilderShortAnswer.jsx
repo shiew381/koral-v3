6d72ecf9303d501180c359d87378d0879270681b
shiew381 <shiew381@gmail.com>
2024-10-06 08:40:08 -0700

    begin work on text with help options question type

--- src/components/forms/QnBuilderShortAnswer.jsx
+++ src/components/forms/QnBuilderShortAnswer.jsx
@@ -15,6 +15,9 @@ import {
   TextField,
   Typography,
   Select,
+  Chip,
+  Tabs,
+  Tab,
 } from "@mui/material";
 import { BtnContainer, SubmitBtn } from "../common/Buttons";
 import { NumberField } from "../common/NumberField";
@@ -122,6 +125,7 @@ export function ShortAnswer({
           value={subtype}
         >
           <MenuItem value={"text"}>Text</MenuItem>
+          <MenuItem value={"text with options"}>Text with Options</MenuItem>
           <MenuItem value={"number"}>Number</MenuItem>
           <MenuItem value={"measurement"}>Measurement</MenuItem>
           <MenuItem value={"chemical formula"}>Chemical Formula</MenuItem>
@@ -147,6 +151,16 @@ export function ShortAnswer({
       <Box sx={{ p: 3 }}>
         {subtype == "text" && (
           <Text
+            edit={edit}
+            handleSubmit={handleSave}
+            scoring={scoring}
+            selQuestion={selQuestion}
+            setScoring={setScoring}
+            submitting={submitting}
+          />
+        )}
+        {subtype == "text with options" && (
+          <TextWithOptions
             edit={edit}
             handleSubmit={handleSave}
             scoring={scoring}
@@ -258,6 +272,194 @@ function Text({
       <Box sx={{ px: "5%" }}>
         <Typography sx={{ mb: 1 }} color="text.secondary">
           Options
+        </Typography>
+
+        {!loading && (
+          <FormControlLabel
+            label="accept alternate capitalization"
+            control={
+              <Checkbox
+                checked={scoring.acceptAltCap}
+                onChange={handleAcceptAltCap}
+              />
+            }
+          />
+        )}
+
+        {!loading && (
+          <FormControlLabel
+            label="accept alternate spacing"
+            control={
+              <Checkbox
+                checked={scoring.acceptAltSpacing}
+                onChange={handleAcceptAltSpacing}
+              />
+            }
+          />
+        )}
+      </Box>
+      <BtnContainer right>
+        <SubmitBtn
+          disabled={submitting}
+          label="SAVE"
+          onClick={(e) => handleSubmit(e, { text: textRef.current.value })}
+          submitting={submitting}
+        />
+      </BtnContainer>
+    </>
+  );
+}
+
+const textOptions = [
+  { value: "mono", category: "quantifiers" },
+  { value: "bis", category: "quantifiers" },
+  { value: "di", category: "quantifiers" },
+  { value: "hexa", category: "quantifiers" },
+  { value: "penta", category: "quantifiers" },
+  { value: "tetra", category: "quantifiers" },
+  { value: "tetrakis", category: "quantifiers" },
+  { value: "tris", category: "quantifiers" },
+  { value: "tri", category: "quantifiers" },
+  { value: "ammine", category: "ligands" },
+  { value: "aqua", category: "ligands" },
+  { value: "carbonyl", category: "ligands" },
+  { value: "cyano", category: "ligands" },
+  { value: "chloro", category: "ligands" },
+  { value: "ethylenediamine", category: "ligands" },
+  { value: "oxalato", category: "ligands" },
+  { value: "oxo", category: "ligands" },
+  { value: "chromium", category: "metals" },
+  { value: "cobalt", category: "metals" },
+  { value: "copper", category: "metals" },
+  { value: "iron", category: "metals" },
+  { value: "(I)", category: "oxidation states" },
+  { value: "(II)", category: "oxidation states" },
+  { value: "(III)", category: "oxidation states" },
+  { value: "(IV)", category: "oxidation states" },
+  { value: "(V)", category: "oxidation states" },
+  { value: "(VI)", category: "oxidation states" },
+];
+
+function TextWithOptions({
+  edit,
+  handleSubmit,
+  scoring,
+  selQuestion,
+  setScoring,
+  submitting,
+}) {
+  const add = !edit;
+  const textRef = useRef();
+  const [loading, setLoading] = useState(true);
+  const categories = textOptions.map((option) => option.category);
+  const uniqueCategories = [...new Set(categories)];
+  const [tabIndex, setTabIndex] = useState(0);
+  const selCategory = uniqueCategories[tabIndex];
+  const shownOptions = textOptions
+    .filter((option) => option.category === selCategory)
+    .map((el) => el.value);
+
+  const defaultScoring = {
+    acceptAltCap: true,
+    acceptAltSpacing: true,
+  };
+
+  function selectTab(e, newIndex) {
+    setTabIndex(newIndex);
+  }
+
+  function handleAcceptAltCap(e) {
+    const scoringCopy = { ...scoring };
+    setScoring({
+      acceptAltCap: e.target.checked,
+      acceptAltSpacing: scoringCopy.acceptAltSpacing,
+    });
+  }
+
+  function handleAcceptAltSpacing(e) {
+    const scoringCopy = { ...scoring };
+    setScoring({
+      acceptAltCap: scoringCopy.acceptAltCap,
+      acceptAltSpacing: e.target.checked,
+    });
+  }
+
+  function handleInsert(textFragment) {
+    const elem = document.getElementById("option-field");
+    const selStart = elem.selectionStart;
+    const selEnd = elem.selectionEnd;
+    const currentText = textRef.current.value;
+    const textBefore = currentText.slice(0, selStart);
+    const textAfter = currentText.slice(selEnd, currentText.length - 1);
+    const newSelPos = textBefore.length + textFragment.length;
+
+    textRef.current.value = textBefore + textFragment + textAfter;
+    elem.focus();
+    elem.setSelectionRange(newSelPos, newSelPos);
+  }
+
+  useEffect(
+    () => {
+      if (add) {
+        setScoring(defaultScoring);
+        textRef.current.value = "";
+      }
+
+      if (edit) {
+        setScoring(selQuestion.scoring);
+        textRef.current.value = selQuestion.correctAnswer.text;
+      }
+    },
+    //eslint-disable-next-line
+    []
+  );
+
+  useEffect(() => setLoading(false), []);
+
+  return (
+    <>
+      <Box sx={{ px: "5%" }}>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Response must match:
+        </Typography>
+
+        <TextField
+          id="option-field"
+          inputRef={textRef}
+          autoComplete="off"
+          fullWidth
+          placeholder="type answer here"
+          variant="filled"
+        />
+      </Box>
+      {/* <input className="text with options field" type="text"></input> */}
+      <br />
+      <Box sx={{ px: "5%" }}>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Helper Options
+        </Typography>
+        <Tabs value={tabIndex} onChange={selectTab} variant="scrollable">
+          {uniqueCategories.map((category) => (
+            <Tab label={category} key={category} />
+          ))}
+        </Tabs>
+
+        <div style={{ paddingTop: "10px" }}>
+          {shownOptions.map((option) => (
+            <Chip
+              label={option}
+              key={option}
+              onClick={() => handleInsert(option)}
+              sx={{ mb: "3px", mr: "3px" }}
+            />
+          ))}
+        </div>
+      </Box>
+      <br />
+      <br />
+      <Box sx={{ px: "5%" }}>
+        <Typography sx={{ mb: 1 }} color="text.secondary">
+          Grading Options
         </Typography>
 
         {!loading && (
